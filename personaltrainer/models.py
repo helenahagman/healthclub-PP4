8f2ec586cbe40cab31ed533f159fc6bf46916cfb
helenahagman <>
2023-08-16 09:59:08 +0000

    added classes for bookingsession and users

--- personaltrainer/models.py
+++ personaltrainer/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.model import User
+from cloudinary.models import CloudinaryFeild
 
-# Create your models here.
+STATUS = ((0, 'previous'), (1, 'Booked'))
+
+
+class BookingSession(models.Model):
+    name = models.CharField(max_length=80, default='Name', blank=False)
+    email = models.EmailField(default='example@example.com', blank=False)
+    age = models.CharField(max_length=2, default='18', blank=False)
+    date = models.DateField(auto_now=True)
+    time = models.TimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, null=True, blank=True)
+
