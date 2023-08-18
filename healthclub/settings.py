f0cf703bd7c8b46a048f137986f2d54625d3d440
helenahagman <>
2023-08-18 07:59:20 +0000

    solved bug in browser not showing, imported dependencies again

--- healthclub/settings.py
+++ healthclub/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-helenahagman-healthclub-rdp0myvwlf.us2.codeanyapp.com']
+ALLOWED_HOSTS = ['8000-helenahagman-healthclub-323qutztyz.us2.codeanyapp.com']
 
 
 # Application definition
@@ -79,12 +79,12 @@ WSGI_APPLICATION = 'healthclub.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
+#    DATABASES = {
+#        'default': {
+#            'ENGINE': 'django.db.backends.sqlite3',
+#            'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
+#        }
 #    }
-# }
 
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
